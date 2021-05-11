7064e39f478f57df280465c12f24c62d9ad38099
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-05-10 20:11:40 -0600

    user can now submit longURL as well as random alpha numerical generator implemented

--- express_server.js
+++ express_server.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 8080; // default port 8080
 app.set('view engine','ejs');
-
+app.use(bodyParser.urlencoded({extended: true}));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+const generateRandomString = function(){
+  let letterArr = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+  let newString = '';
+  for (let i = 0; i < 6; i++) {
+    let randomNum = Math.floor(Math.random() * 58);
+   newString += letterArr[randomNum];
+  }
+  return newString;
+};
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -20,6 +30,10 @@ app.get('/urls',(req, res) => {
 });
 app.get('/urls/new',(req,res) => {
   res.render('urls_new');
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
